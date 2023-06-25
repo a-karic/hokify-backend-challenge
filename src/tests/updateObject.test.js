ed14202b940f90f1fd52ab051ba16a1f2f0ca257
Almin Karic <almin.karic@workflow.at>
2023-06-25 18:49:02 +0200

    Add updateObject tests with the Hokify specs
    This commit adds several test cases for different scenarios of updating,
    creating, and removing elements of objects and arrays. The function and
    tests are based on the specs provided by Hokify.

--- src/tests/updateObject.test.js
+++ src/tests/updateObject.test.js
@@ -225,48 +225,385 @@ describe('updateObject function', () => {
     expect(result).toEqual(expected);
   });
 
-  // Cases from https://gist.github.com/simllll/a130e1c81c66ab1a3c3cdb51025f95df#file-hokify-backend-coding-challenge1-md
+});
+
+describe('updateObject function - tests from Hokify', () => {
+  let object;
+
+  beforeEach(() => {
+    object = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      value: 'hui',
+    };
+  });
+
   it('should update element of array by string path a.b[_id] and overwrite the whole object except _id', () => {
-    // TODO impl using given/when/then
+    // Given
+    const input = {
+      'a.b[5dc0ad700000000000000000]': { title: 'asdf1-updated' },
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', title: 'asdf1-updated' }, // name got removed, all other objects got overwritten, _id is still automatically applied though
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should update element of array by string path a.b[_id].element and add or overwrite the nested value', () => {
-    // TODO impl using given/when/then
+  it('should update element of array by string path a.b[_id].titleValue and add or overwrite the value', () => {
+    // Given
+    const input = {
+      'a.b[5dc0ad700000000000000000].titleValue': 'asdf1-updated',
+    };
+
+    const expected = {
+      a: {
+        b: [
+          {
+            _id: '5dc0ad700000000000000000',
+            name: 'asdf1',
+            titleValue: 'asdf1-updated',
+          }, // titleValue got added / overwritten (if it was there already). all other values remain the same
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should add a new element to array by string path a.b[] with _id and name', () => {
-    // TODO impl using given/when/then
+  it('should add an element to array by string path a.b[] with a new object', () => {
+    // Given
+    const input = {
+      'a.b[]': { _id: '5dc0ad700000000000000003', name: 'co2' },
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+          { _id: '5dc0ad700000000000000003', name: 'co2' }, // this entry has been added
+        ],
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should remove element of array by string path a.b[_id] with null value', () => {
-    // TODO impl using given/when/then
-  });
-  
-  it('should add a new value to object by string path a.c with "hallo"', () => {
-    // TODO impl using given/when/then
+  it('should remove an element from array by string path a.b[_id] with null value', () => {
+    // Given
+    const input = {
+      'a.b[5dc0ad700000000000000001]': null,
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          // asdf2- id has been removed
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should update value of object by string path a.c with "hallo-changed"', () => {
-    // TODO impl using given/when/then
+  it('should add a regular object value by string path a.c', () => {
+    // Given
+    const input = {
+      'a.c': 'hallo',
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+        c: 'hallo', // <-- this property has been added
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should remove value of object by string path value with null value', () => {
-    // TODO impl using given/when/then
+  it('should update a regular object value by string path a.c', () => {
+    // Given
+    const input = {
+      'a.c': 'hallo-changed',
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+        c: 'hallo-changed', // <-- this property has been updated
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should remove value of object by string path a.b with null value', () => {
-    // TODO impl using given/when/then
+  it('should unset a regular object value on root level by string path value with null value', () => {
+    // Given
+    const input = {
+      value: null,
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      // value is not here anymore
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should apply multiple updates by string paths value, seperated by ","', () => {
-    // TODO impl using given/when/then
+  it('should unset a regular object value NOT on root level by string path a.b with null value', () => {
+    // Given
+    const input = {
+      'a.b': null,
+    };
+
+    const expected = {
+      a: {
+        // b is not here anymore
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should create new paths in object or array by string paths x[], v.x[] and v.m.l with "asdfX", "asdfV" and "asdf-val" values', () => {
-    // TODO impl using given/when/then
+  it('should apply multiple operations at one time by string paths with different values', () => {
+    // Given
+    const input = {
+      value: null,
+      something: 'anything',
+      'a.c': 'hallo',
+    };
+
+    const expected = {
+      a: {
+        c: 'hallo', // 3. a.c is set to hallo
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      something: 'anything', // 2. something with value anything
+      // 1. value has been removed
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
 
-  it('should update value of object by string path images with new image urls', () => {
-    // TODO impl using given/when/then
+  it('should apply array update and create underlying array or object by string paths with different values', () => {
+    // Given
+    const input = {
+      'x[]': 'asdfX',
+      'v.x[]': 'asdfV',
+      'v.m.l': 'asdf-val',
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      x: ['asdfX'], // empty array x has been created, and asdfX has been added.
+      v: {
+        // v has been automatically created as object
+        x: ['asdfV'], // underlying x has been created as array
+        m: {
+          // m has been created automatically as object
+          l: 'asdf-val', // value of l has been set to asdf-val
+        },
+      },
+      value: 'hui',
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
   });
+
+  it('should update the images object by string path images with a new object', () => {
+    // Given
+    const input = {
+      images: {
+        thumbnail:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        small:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        medium:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        large:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        xlarge:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+      },
+    };
+
+    const expected = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      value: 'hui',
+      images: {
+        thumbnail:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        small:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        medium:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        large:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+        xlarge:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304827.jpg',
+      },
+    };
+
+    // When
+    const result = updateObject(object, input);
+
+    // Then
+    expect(result).toEqual(expected);
+
+    // Given
+    const inputThumbnailChanged = {
+      'images.thumbnail':
+        'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304828.jpg',
+    };
+
+    const expectedThumbnailChanged = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      value: 'hui',
+      images: {
+        thumbnail:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573480304828.jpg',
+        small:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573477587288.jpg',
+        medium:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573477587288.jpg',
+        large:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573477587288.jpg',
+        xlarge:
+          'http://files-test.hokify.com/user/pic_5b30ac932c6ba6190bfd7eef_1573477587288.jpg',
+      },
+    };
+
+    // When
+    const resultThumbnailChanged = updateObject(result, inputThumbnailChanged);
+
+    // Then
+    expect(resultThumbnailChanged).toEqual(expectedThumbnailChanged);
+
+    // Given
+    const inputDeleteImages = {
+      images: null,
+    };
+
+    const expectedDeleteImages = {
+      a: {
+        b: [
+          { _id: '5dc0ad700000000000000000', name: 'asdf1' },
+          { _id: '5dc0ad700000000000000001', name: 'asdf2' },
+          { _id: '5dc0ad700000000000000002', name: 'asdf3' },
+        ],
+      },
+      value: 'hui',
+      // images is not here anymore
+    };
+
+    // When
+    const resultDeleteImages = updateObject(resultThumbnailChanged, inputDeleteImages);
+
+    // Then
+    expect(resultDeleteImages).toEqual(expectedDeleteImages);
+  });
+
 });
